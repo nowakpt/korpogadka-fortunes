21aad44f78f8d1651546ef8d1caebed79d069745
Patryk Nowak <nowak.p92@gmail.com>
2018-08-27 20:44:50 +0200

    Add more verbose output for update_cows.sh

--- update_cows.sh
+++ update_cows.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ls /usr/share/cowsay/cows/ | grep '\.cow$' | sed s/'.cow$'// > cows.txt
+ls /usr/share/cowsay/cows/ | grep '\.cow$' | sed s/'.cow$'// > cows.txt && echo 'List of cows has been updated.'
 
