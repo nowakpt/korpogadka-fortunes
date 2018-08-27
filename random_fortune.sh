75c89efae4808a0c431167a7dbd4856a19a8b275
Patryk Nowak <nowak.p92@gmail.com>
2018-08-27 20:46:22 +0200

    random_fortune.sh: replace sleep with read-with-timeout

--- random_fortune.sh
+++ random_fortune.sh
@@ -6,5 +6,5 @@ INTERVAL=8  # seconds
 
 [ -n "$1" ] && INTERVAL="$1"
 
-while true ; do clear; shuf -n 1 $INPUT_FILE | cowsay -f $(shuf -n 1 cows.txt) | lolcat ; sleep $INTERVAL ; done
+while true ; do clear; shuf -n 1 $INPUT_FILE | cowsay -f $(shuf -n 1 cows.txt) | lolcat ; read -t $INTERVAL ; done
 
