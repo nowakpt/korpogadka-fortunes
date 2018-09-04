2657d5015b44ee2cc5028dd4b8154bef4e7376b4
Patryk Nowak <nowak.p92@gmail.com>
2018-09-04 15:21:05 +0200

    Allow reading multiple files with fortunes

--- random_fortune.sh
+++ random_fortune.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-INPUT_FILE='fortunes.txt'
+INPUT_FILES="fortunes*.txt"
 
 INTERVAL=8  # seconds
 
 COWS=$(cowsay -l | tail -n +2)
+FORTUNES=$(cat $INPUT_FILES)
 
 [ -n "$1" ] && INTERVAL="$1"
 
 while true ; do
 	COW=$(echo $COWS | tr ' ' '\n' | shuf -n 1)
+	FORTUNE=$(echo "$FORTUNES" | shuf -n 1)
 	clear
-	shuf -n 1 $INPUT_FILE | cowsay -f $COW | lolcat
+	echo -e $FORTUNE | cowsay -f $COW | lolcat
 	read -t $INTERVAL
 done
 
